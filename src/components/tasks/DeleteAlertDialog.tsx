a6fd10a72f95984cd945b64591a45fafc354ffea
Rohan Shrestha <shrestharohan039@gmail.com>
2022-12-20 19:23:40 +0545

    changed colors and deadline in seed data

--- src/components/tasks/DeleteAlertDialog.tsx
+++ src/components/tasks/DeleteAlertDialog.tsx
@@ -7,37 +7,37 @@ interface Props {
   children: ReactNode;
 }
 
-export default function DeleteAlertDialog(props: Props) {
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>{props.children}</AlertDialog.Trigger>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="overlay" />
-        <AlertDialog.Content className="flex flex-col gap-5 py-5 px-7 modal-content">
-          <AlertDialog.Cancel className="absolute top-[22px] right-7">
-            <CrossLottie />
+const DeleteAlertDialog = (props: Props) => (
+  <AlertDialog.Root>
+    <AlertDialog.Trigger>{props.children}</AlertDialog.Trigger>
+    <AlertDialog.Portal>
+      <AlertDialog.Overlay className="overlay" />
+      <AlertDialog.Content className="flex flex-col gap-5 py-5 px-7 modal-content">
+        <AlertDialog.Cancel className="absolute top-[22px] right-7">
+          <CrossLottie />
+        </AlertDialog.Cancel>
+        <AlertDialog.Title className="text-lg font-medium">
+          Are you absolutely sure?
+        </AlertDialog.Title>
+        <AlertDialog.Description className="text-[15px] dark:text-stone-400 text-stone-600">
+          This action cannot be undone. This will permanently delete your data
+          and remove your data from our servers.
+        </AlertDialog.Description>
+        <div className="flex self-end gap-8">
+          <AlertDialog.Action asChild onClick={props.clickHandler}>
+            <button className="px-4 py-2 font-medium rounded shadow text-rose-800 bg-rose-200 hover:bg-rose-300">
+              Yes, delete data
+            </button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel asChild>
+            <button className="font-medium rounded shadow btn-primary">
+              Cancel
+            </button>
           </AlertDialog.Cancel>
-          <AlertDialog.Title className="text-lg font-medium">
-            Are you absolutely sure?
-          </AlertDialog.Title>
-          <AlertDialog.Description className="text-[15px] dark:text-stone-400 text-stone-600">
-            This action cannot be undone. This will permanently delete your data
-            and remove your data from our servers.
-          </AlertDialog.Description>
-          <div className="flex self-end gap-8">
-            <AlertDialog.Action asChild onClick={props.clickHandler}>
-              <button className="px-4 py-2 font-medium rounded shadow text-rose-800 bg-rose-200 hover:bg-rose-300">
-                Yes, delete data
-              </button>
-            </AlertDialog.Action>
-            <AlertDialog.Cancel asChild>
-              <button className="font-medium rounded shadow btn-primary">
-                Cancel
-              </button>
-            </AlertDialog.Cancel>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
-  );
-}
+        </div>
+      </AlertDialog.Content>
+    </AlertDialog.Portal>
+  </AlertDialog.Root>
+);
+
+export default DeleteAlertDialog;
--- src/lib/seedDefaultData.ts
+++ src/lib/seedDefaultData.ts
@@ -1,46 +1,48 @@
 import prisma from './prismadb';
 
-export default async function seedDefaultData(id: string) {
+const getDate = (num: number) => {
   const deadline = new Date();
-  deadline.setDate(deadline.getDate() + 1);
-  const tagColor = '#93c5fd';
+  deadline.setDate(deadline.getDate() + num);
+  return deadline;
+};
 
-  const defaultData = [
-    {
-      heading: 'To Do',
-      task: {
-        title: 'Support Small Screens',
-        description:
-          'Make website responsive for mobiles, tablets and other small screens',
-        tagTitle: 'Support',
-        tagColor,
-        deadline,
-      },
+const defaultData = [
+  {
+    heading: 'To Do',
+    task: {
+      title: 'Support Small Screens',
+      description:
+        'Make website responsive for mobiles, tablets and other small screens',
+      tagTitle: 'Support',
+      tagColor: '#86efac',
+      deadline: getDate(3),
     },
-    {
-      heading: 'Doing',
-      task: {
-        title: 'Add Animations',
-        description:
-          'Animate dropdowns, modals, cards, page etc using Framer Motion',
-        tagTitle: 'UX/UI Design',
-        tagColor,
-        deadline,
-      },
+  },
+  {
+    heading: 'Doing',
+    task: {
+      title: 'Add Animations',
+      description:
+        'Animate dropdowns, modals, cards, page etc using Framer Motion',
+      tagTitle: 'UX/UI Design',
+      tagColor: '#93c5fd',
+      deadline: getDate(2),
     },
-    {
-      heading: 'In Review',
-      task: {
-        title: 'Calendar Features',
-        description:
-          'Improve user experience in the calendar page by adding features',
-        tagTitle: 'Features',
-        tagColor,
-        deadline,
-      },
+  },
+  {
+    heading: 'In Review',
+    task: {
+      title: 'Calendar Features',
+      description:
+        'Improve user experience in the calendar page by adding features',
+      tagTitle: 'Features',
+      tagColor: '#f9a8d4',
+      deadline: getDate(1),
     },
-  ];
+  },
+];
 
+export default async function seedDefaultData(id: string) {
   await prisma.user.update({
     where: { id },
     data: {
